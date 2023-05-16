399284957402212c84145db140422747d2eabc38
Sameh ibrahim <133765531+SamehMO22@users.noreply.github.com>
2023-05-17 00:53:11 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB  = async ()=>{
     console.log(process.env.DB_CLOUD);
-    return await mongoose.connect(process.env.DB_LOCAL)
+    return await mongoose.connect(process.env.DB_CLOUD)
     .then(res=>console.log(process.env.DB_CLOUD))
     .catch(err=>console.log(` Fail to connect  DB.........${err} `))
 }   
